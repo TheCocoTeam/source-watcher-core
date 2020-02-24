c908dfec12c76f498a67700a2f72e442dfc9d3cb
Jean Paul Ruiz <Jean Paul Ruiz jeanpaul.ruiz@hitachivantara.com>
2020-02-24 11:44:22 -0500

    adding partial refactoring
    adding code cleanup
    
    moving to native dom logic

---
+++ src/Vendors/StackOverflow/StackOverflowWebPageHandler.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Coco\SourceWatcher\Vendors\StackOverflow;
+
+use Coco\SourceWatcher\Handler\WebPageHandler;
+use DOMXPath;
+
+class StackOverflowWebPageHandler extends WebPageHandler {
+    public function __construct ( string $url ) {
+        parent::__construct( $url );
+    }
+
+    private array $results;
+
+    public function getResults () : array {
+        return $this->results;
+    }
+
+    public function read () : void {
+        parent::read();
+
+        $this->results = array();
+
+        $dom = parent::getDom();
+
+        $finder = new DomXPath($dom);
+        $classname = "listResults";
+        $listResultsDom = $finder->query( "//*[contains(@class, '$classname')]" ); // DOMNodeList
+
+        for ( $i = 0; $i < $listResultsDom->count(); $i++ ) {
+            $currentDomNode = $listResultsDom->item( $i ); // DOMNode
+
+            if ( $currentDomNode->hasChildNodes() ) {
+                $children = $currentDomNode->childNodes; // DOMNodeList
+                print_r( $children );
+
+                for ( $j = 0; $j < $children->count(); $j++ ) {
+                    $currentChildrenNode = $children->item( $j ); // DOMNode
+                    //print_r( $currentChildrenNode );
+
+                    $attributes = $currentChildrenNode->attributes; // DOMNamedNodeMap
+
+                    if ( $attributes != null ) {
+                        $currentJob = new StackOverflowJob();
+
+                        foreach ( $attributes as $currentAttribute ) {
+                            //print_r( $currentAttribute );
+
+                            if ( $currentAttribute->name != null ) {
+                                if ( $currentAttribute->name == "data-jobid" ) {
+                                    $currentJob->setJobId( $currentAttribute->value );
+                                }
+
+                                if ( $currentAttribute->name == "data-result-id" ) {
+                                    $currentJob->setResultId( $currentAttribute->value );
+                                }
+
+                                if ( $currentAttribute->name == "data-preview-url" ) {
+                                    $currentJob->setPreviewUrl( $currentAttribute->value );
+                                }
+                            }
+                        }
+
+                        if ( $currentJob->allAttributesDefined() ) {
+                            array_push( $this->results, $currentJob );
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
