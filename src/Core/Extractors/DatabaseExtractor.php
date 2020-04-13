6044ccc2186e4bb5b1099022e47bdb98d4a44782
Jean Paul Ruiz <jpruiz114@gmail.com>
2020-04-13 19:35:10 -0400

    Adding database extractor class

---
+++ src/Core/Extractors/DatabaseExtractor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Coco\SourceWatcher\Core\Extractors;
+
+use Coco\SourceWatcher\Core\Extractor;
+
+/**
+ * Class DatabaseExtractor
+ * @package Coco\SourceWatcher\Core\Extractors
+ */
+class DatabaseExtractor extends Extractor
+{
+    /**
+     * @inheritDoc
+     */
+    public function extract ()
+    {
+        // TODO: Implement extract() method.
+    }
+}
