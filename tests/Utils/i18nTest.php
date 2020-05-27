41a6546c0fde53e788103cb8d6dbf057779a3f7b
Jean Paul Ruiz <jpruiz114@gmail.com>
2020-05-26 21:44:48 -0400

    Adding extra configuration for the unit tests of the i18n class

--- tests/Utils/i18nTest.php
+++ tests/Utils/i18nTest.php
@@ -12,17 +12,77 @@ use PHPUnit\Framework\TestCase;
 class i18nTest extends TestCase
 {
     /**
+     * @var string
+     */
+    private string $envFileLocation = __DIR__ . "/../../" . ".env";
+
+    /**
+     * @var bool
+     */
+    private bool $preExistingEnvFile = false;
+
+    /**
+     * @var bool[][]
+     */
+    private array $envConditionsPerFunction = [ "testGetInstance" => [ "add_i18n_property" => true ], "testGetText" => [ "add_i18n_property" => true ], "testNoEnvFileDefaultToEnglish" => [ "add_i18n_property" => false ] ];
+
+    /**
+     *
+     */
+    public function setUp () : void
+    {
+        $nextFunctionToBeCalled = $this->getName();
+
+        if ( file_exists( $this->envFileLocation ) ) {
+            $this->preExistingEnvFile = true;
+        } else {
+            $addi18nProperty = $this->envConditionsPerFunction[$nextFunctionToBeCalled]["add_i18n_property"];
+
+            $envFile = fopen( $this->envFileLocation, "w" );
+
+            if ( $addi18nProperty ) {
+                $i18nSetting = "I18N_LANGUAGE=en_US";
+                fwrite( $envFile, $i18nSetting );
+            }
+
+            fclose( $envFile );
+        }
+    }
+
+    /**
+     *
+     */
+    public function tearDown () : void
+    {
+        if ( !$this->preExistingEnvFile ) {
+            unlink( $this->envFileLocation );
+        }
+    }
+
+    /**
      *
      */
     public function testGetInstance () : void
     {
-        $this->assertEquals( i18n::getInstance(), i18n::getInstance() );
+        $this->assertEquals( new i18n(), i18n::getInstance() );
     }
 
     /**
      *
      */
     public function testGetText () : void
+    {
+        $expectedString = "This is a test entry!";
+        $actualString = i18n::getInstance()->getText( i18nTest::class, "This_Is_A_Test_Entry" );
+
+        $this->assertNotNull( $actualString );
+        $this->assertEquals( $expectedString, $actualString );
+    }
+
+    /**
+     *
+     */
+    public function testNoEnvFileDefaultToEnglish () : void
     {
         $expectedString = "This is a test entry!";
         $actualString = i18n::getInstance()->getText( i18nTest::class, "This_Is_A_Test_Entry" );
