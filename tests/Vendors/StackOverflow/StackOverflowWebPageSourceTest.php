2ff9a413f64504b86b516f6b8b7361ae9d6b8915
Jean Paul Ruiz <jpruiz114@gmail.com>
2020-08-29 21:21:48 -0400

    Adding unit test to StackOverflow web page source for change of URL after instance

--- tests/Vendors/StackOverflow/StackOverflowWebPageSourceTest.php
+++ tests/Vendors/StackOverflow/StackOverflowWebPageSourceTest.php
@@ -17,4 +17,17 @@ class StackOverflowWebPageSourceTest extends TestCase
         $this->assertNotNull( $results );
         $this->assertNotEmpty( $results );
     }
+
+    public function testGetResultsChangeUrlAfterInstance () : void
+    {
+        $url = "https://stackoverflow.com/jobs?q=PHP&l=Florida+USA&d=100&u=Miles";
+        $webPageSource = new StackOverflowWebPageSource( $url );
+
+        $url = "https://stackoverflow.com/jobs?q=Java&l=Florida+USA&d=100&u=Miles";
+        $webPageSource->setUrl( $url );
+
+        $results = $webPageSource->getResults();
+        $this->assertNotNull( $results );
+        $this->assertNotEmpty( $results );
+    }
 }
